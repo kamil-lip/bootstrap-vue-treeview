dfe150ac56e858b3a05456842ce6d25129643350
Kamil Lipiński <kamil@appdiamond.pl>
2018-05-17 14:37:35 +0200

    Add main vue instance to the dev script.

--- dev/public/index.js
+++ dev/public/index.js
@@ -1,2 +1,6 @@
 // This is not the best way to go with the tree component. I recommend you to use webpack with babel as described in the README file.
 Vue.use(bootstrap4VueTreeview['default']);
+
+var app = new Vue({
+  el: '#app'
+})
