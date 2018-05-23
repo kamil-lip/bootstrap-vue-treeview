f8d4f81c83eb75b0964337d7f4dae58ec3b10588
Kamil Lipiński <kamil@appdiamond.pl>
2018-05-23 14:35:49 +0200

    Expand/collapse node's children after clicking node arrow icon.

--- src/components/TreeNode.vue
+++ src/components/TreeNode.vue
@@ -1,13 +1,13 @@
 <template> 
 
 <div class="tree-node">
-	<svg width="12" height="12">
+	<svg width="12" height="12" @click.prevent="toggle">
 		<path
 		 d="M2 1 L10 6 L2 11 Z"
 		 style="opacity:1;fill:currentColor;stroke:currentColor;stroke-width:2;stroke-linecap:round;stroke-linejoin:round;stroke-miterlimit:4;stroke-dasharray:none;stroke-opacity:1" />
 	</svg>
 	<span>{{ data.name }}</span>
-	<div class="tree-node-children" v-if="data.children">
+	<div class="tree-node-children" v-if="expanded && data.children && Array.isArray(data.children)">
 		<tree-node v-for="nodeData in data.children" :data="nodeData"></tree-node>
 	</div>
 </div>
@@ -24,6 +24,16 @@ export default {
 		data: {
 			type: Object,
 			required: true
+		}
+	},
+	data() {
+		return {
+			expanded: false
+		}
+	},
+	methods: {
+		toggle() {
+			this.expanded = !this.expanded
 		}
 	}
 }
