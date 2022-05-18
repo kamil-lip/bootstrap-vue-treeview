a809b6befd816e1c88f4cf3891397c365108291e
quanganhats <56571929+quanganhats@users.noreply.github.com>
2022-05-18 11:56:44 +0700

    fix: property open action itemmenu

--- src/components/ContextMenu.vue
+++ src/components/ContextMenu.vue
@@ -1,51 +1,54 @@
 <template>
-
-<vue-context-menu id="context-menu" ref="ctxMenu">
-  <li class="ctx-item" @click.stop.prevent="menuItemSelected(item)" v-for="item in contextMenuItems">{{ item.label }}</li>
-</vue-context-menu>
-
+  <vue-context-menu id="context-menu" ref="ctxMenu">
+    <li
+      class="ctx-item"
+      @click.stop.prevent="menuItemSelected(item)"
+      v-for="item in contextMenuItems"
+      :key="item + i"
+    >
+      {{ item.label }}
+    </li>
+  </vue-context-menu>
 </template>
 
 <script>
-
-import EventBus from '../EventBus';
-import VueContextMenu from 'vue-context-menu'
+import EventBus from "../EventBus";
+import VueContextMenu from "vue-context-menu";
 
 export default {
   components: {
-    VueContextMenu
+    VueContextMenu,
   },
   props: {
     contextMenuItems: {
-      type: Array
-    }
+      type: Array,
+    },
   },
   data() {
     return {
-      activeNode: null
-    }
+      activeNode: null,
+    };
   },
   methods: {
     open(node) {
-      this.activeNode = node
-      this.$refs.ctxMenu.open()
+      this.activeNode = node;
+      if (this.$refs.ctxMenu != null) {
+        this.$refs.ctxMenu.open();
+      }
     },
     menuItemSelected(item) {
-      EventBus.$emit('contextMenuItemSelect', item, this.activeNode)
-    }
+      EventBus.$emit("contextMenuItemSelect", item, this.activeNode);
+    },
   },
   created() {
-    EventBus.$on('openNodeContextMenu', this.open)
-  }
-}
-
+    EventBus.$on("openNodeContextMenu", this.open);
+  },
+};
 </script>
 
 <style>
-
 .ctx-item {
   cursor: pointer;
   user-select: none;
 }
-
 </style>
